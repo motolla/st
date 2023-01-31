ccd27d8ace909a631b54fab5a30f67db6c624894
gillgrite <gillgrite@gmail.com>
2023-01-31 23:24:59 +0700

    mouse scroll

--- config.def.h
+++ config.def.h
@@ -193,6 +193,8 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
   /* mask                 button   function        argument       release */
+	{ ShiftMask,            Button4, kscrollup,      {.i = 1} },
+	{ ShiftMask,            Button5, kscrolldown,    {.i = 1} },
   { XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
   { ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
   { XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
@@ -220,8 +222,6 @@ static Shortcut shortcuts[] = {
   { TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
   { ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
   { ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
-  { ShiftMask,            Button4,        kscrollup,      {.i = 1} },
-  { ShiftMask,            Button5,        kscrolldown,    {.i = 1} },
 };
 
 /*
